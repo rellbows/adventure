336eea5a65c400ec769617df24f210cf38dbf1ab
ellisry <ellisry@oregonstate.edu>
2019-02-09 14:14:13 -0800

    Directory creation code completed.

--- ellisry.buildrooms.c
+++ ellisry.buildrooms.c
@@ -1,21 +1,61 @@
 // TODO: header here...
 
-#include <stdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+// The rooms (from Clue!)
+const char* rooms[] = {"Ballroom", "Billard Room", "Conservatory", "Dining Room", "Hall", "Kitchen", "Library", "Lounge", "Study", "Bathroom"};
 
 int main(){
 
-	// Get PID for process
-	
-	// Make <username>.rooms.<pid> directory
+	// Variable declaration
+	int pid = 0;    // Holds process id
+	char* directory = malloc(25 * sizeof(char));    // Holds directory name; should be plenty of room
+	memset(directory, '\0', 25);    // Clearing string
+	char* firstDir = malloc(15 * sizeof(char));    // First part of directory
+	memset(firstDir, '\0', 15);
+	strcpy(firstDir, "ellisry.rooms.");	
+	char* strPid = malloc(11 * sizeof(char));    // Holds pid as string
+	memset(strPid, '\0', 11);    // Clearing...
+	int dirResult = 0;
 
+	// Get PID for process
+	pid = getpid();
+	// Testing
+	printf("%d\n", pid);
+	// Convert PID from int to string
+	snprintf(strPid, 11, "%d", pid);
+	// Testing
+	printf("%s\n", strPid);
+	
+	// Make string for <username>.rooms.<pid> directory
+	snprintf(directory, 25, "%s%s", firstDir, strPid);
+	// Testing
+	printf("%s\n", directory);
+
+	// Make actual directory
+	dirResult = mkdir(directory, 0755);
+	// Check result of call
+	if(dirResult != 0){
+		perror("Error creating directory!");
+		return(-1);
+	}
+	/*
 	// Create all connections in graph
 	while (IsGraphFull() == false){
 		AddRandomConnection();
 	}
+	*/
+
+	// Cleanup memory allocation
+	free(directory);
 
 	return 0;
 }
 
+/*
 // Returns true if all rooms have 3 to 6 outbound connections
 bool IsGraphFull(){
 	// TODO: need to start
@@ -61,4 +101,4 @@ void ConnectRoom(Room x, Room y){
 // Returns true if Rooms x and y are the same Room, false otherwise
 bool IsSameRoom(Room x, Room y){
 	// TODO: complete...
-}
+}*/
